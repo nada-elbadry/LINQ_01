0aa21f06e37b3d91995cca351dbe5d72648e89e9
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-09-06 23:28:48 +0300

    My Assigment

---
+++ Demo_LINQ_01/Data/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_LINQ_01.Data
+{
+    internal class Customer
+    {
+    }
+}
